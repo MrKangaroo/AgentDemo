3cd3a8914f524183c5229c6cb5b21b06271ea6c2
daisy <daisy01@missfresh.cn>
2019-01-05 22:38:55 +0800

    fitst commit

---
+++ src/main/java/com/d1989/agent/AgentTest.java
@@ -0,0 +1,14 @@
+package com.d1989.agent;
+
+/**
+ * @author daisy
+ * @desc
+ * @create 2019/1/5
+ */
+public class AgentTest {
+
+    public static void main(String[] args) {
+        System.out.println("this is main");
+    }
+
+}
